ab344de4b59fd62846a493e901633fc2c9aaf367
Duxkyx <55333354+duxkyx@users.noreply.github.com>
2022-12-27 02:49:36 +0000

    Create ghub.lua

---
+++ Libs/ghub.lua
@@ -0,0 +1,14 @@
+getgenv().fontsize = 15
+getgenv().titlesize = 15
+getgenv().font = Enum.Font.Code
+getgenv().background = "rbxassetid://6880496154"
+getgenv().backgroundcolour = Color3.fromRGB(20, 20, 20)
+getgenv().cursor = true
+getgenv().outlinecolor = Color3.fromRGB(80, 80, 80)
+getgenv().toptextcolor = Color3.fromRGB(120, 120, 120)
+getgenv().itemscolor = Color3.fromRGB(120, 120, 120)
+getgenv().accentcolour = Color3.fromRGB(66, 74, 79)
+
+
+local library = loadstring(game:HttpGet("https://duxkyx.cf/Roblox/UiLibs/library.lua", true))()
+return library
