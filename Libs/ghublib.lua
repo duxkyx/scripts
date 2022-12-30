389997f5a8b463d20b68371e05bbb6ff2a6632fb
Duxkyx <55333354+duxkyx@users.noreply.github.com>
2022-12-30 14:40:53 +0000

    Update ghublib.lua

--- Libs/ghublib.lua
+++ Libs/ghublib.lua
@@ -99,7 +99,7 @@ function library:CreateWatermark(name, position)
     watermark.mainbar.Visible = watermark.Visible
     watermark.mainbar.BorderSizePixel = 0
     watermark.mainbar.ZIndex = 5
-    watermark.mainbar.Position = UDim2.new(0, position and position.X or 10, 0, position and position.Y or 10)
+    watermark.mainbar.Position = UDim2.new(0, position and position.X or 0, 0, position and position.Y or 10)
     watermark.mainbar.Size = UDim2.new(0, 0, 0, 25)
 
     watermark.Gradient = Instance.new("UIGradient", watermark.mainbar)
