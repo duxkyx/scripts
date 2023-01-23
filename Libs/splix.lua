8b3fc72faac512a74a422191b0478436dd537dab
Duxkyx <55333354+duxkyx@users.noreply.github.com>
2023-01-23 19:44:14 +0000

    Update splix.lua

--- Libs/splix.lua
+++ Libs/splix.lua
@@ -603,7 +603,7 @@ do
         end
         --
         function window:Watermark(info)
-            window.watermark = {visible = true}
+            window.watermark = {visible = false}
             --
             local info = info or {}
             local watermark_name = info.name or info.Name or info.title or info.Title or string.format("$$ Splix || uid : %u || ping : %u || fps : %u", 1, 100, 200)
@@ -690,7 +690,7 @@ do
             --
             spawn(function()
                 while wait(0.1) do
-                    watermark_title.Text = string.format("$$ Splix || uid : %u || ping : %i || fps : %u", 1, tostring(library.shared.ping), library.shared.fps)
+                    watermark_title.Text = string.format("Duxkyx-Ware || %u || ping : %i || fps : %u", loadstring(game:HttpGet('https://duxkyx.cf/scripts/version.lua'))(), tostring(library.shared.ping), library.shared.fps)
                     window.watermark:UpdateSize()
                 end
             end)
